e7d9819eb2331846e2d3bf48c3f663f676720885
fdwillis <fdwillis7@gmail.com>
2015-05-23 18:46:32 -0500

    added caching eveyrything set

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -1,5 +1,6 @@
 require 'stripe'
 class SubscribeController < ApplicationController
+  caches_page :new
   def new
     unless (params[:plan_name] == "basic" || params[:plan_name] == "advanced" || params[:plan_name] == "standard")
       flash[:notice] = "Please select a plan to get started."
@@ -7,7 +8,6 @@ class SubscribeController < ApplicationController
     end
   end
   def update
-    debugger
     token = params[:stripeToken]
     customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
