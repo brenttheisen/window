9dea1af89888e084381bc8dd787cc3dc3007d1c4
fdwillis <fdwillis7@gmail.com>
2015-05-25 18:56:55 -0500

    CRUD for contacts complete. build backend for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :contacts
+
   root to: 'home#index'
   resources :subscribe
   resources :shippings
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525224554) do
+ActiveRecord::Schema.define(version: 20150525233950) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shippings", force: :cascade do |t|
     t.string   "email"
