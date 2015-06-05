a0c39a5ee9982f55cfa6a8533d30ac414a12b9a1
fdwillis <fdwillis7@gmail.com>
2015-06-05 15:47:09 -0500

    about to add the social promo model

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -1,12 +1,14 @@
 require 'stripe'
 class SubscribeController < ApplicationController
   caches_page :new
+  
   def new
     unless (params[:plan_name] == "window" || params[:plan_name] == "world" || params[:plan_name] == "business")
       flash[:notice] = "Please select a plan to get started."
       redirect_to root_path
     end
   end
+
   def update
     token = params[:stripeToken]
     customer = Stripe::Customer.create(
