a3d392db18fde0aaa93b6a2c02094c296764691f
fdwillis <fdwillis7@gmail.com>
2015-07-02 19:03:00 -0500

    dropdown working on all pages now, business and band donation pages up

--- app/models/biz_promo.rb
+++ app/models/biz_promo.rb
@@ -1,4 +1,5 @@
 class BizPromo < ActiveRecord::Base
   has_attached_file :logo, styles: { medium: "300x300#", thumb: "100x100>#" }, default_url: "https://cdn3.iconfinder.com/data/icons/abstract-1/512/no_image-512.png"
-  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :logo, { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+  validates :business_name, :logo, presence: true
 end
