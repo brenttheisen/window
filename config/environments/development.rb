296dec6dcd7e2f83ef7efcd999e47598c2b16af0
fdwillis <fdwillis7@gmail.com>
2015-07-12 22:12:00 -0500

    added error page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,5 +43,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.consider_all_requests_local = false
 
 end
